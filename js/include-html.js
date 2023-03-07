19a22e1c979e4bb729f2ea46d3ef180a01231fe9
Matheus Juliao <matheusjosejuliao@gmail.com>
2023-03-07 20:27:56 -0300

    Create project - Navbar, carolsel and footer

---
+++ js/include-html.js
@@ -0,0 +1,27 @@
+(function includeHTML() {
+    let z, i, element, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      element = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = element.getAttribute("include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {element.innerHTML = this.responseText;}
+            if (this.status == 404) {element.innerHTML = "Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            element.removeAttribute("include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    }
+}());
