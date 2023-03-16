8015d29f903868d897a41f327a8b49d856014c4c
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-03-16 12:14:53 -0300

    Add effcts map

--- js/index.js
+++ js/index.js
@@ -18,3 +18,25 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+let showEffects = true
+
+function effects() {
+  let card = document.getElementsByClassName("card")[0];
+  let container = document.getElementsByClassName("container-arrow")[0];
+
+  if(showEffects) {
+    card.classList.add('effects-transiton')
+    card.getElementsByTagName('h1')[0].style.display = "none";
+    card.getElementsByTagName('p')[0].style.display = "none"
+    container.classList.add('container-arrow-after')
+
+    showEffects = false
+  } else {
+      card.classList.remove('effects-transiton')
+      card.getElementsByTagName('h1')[0].style.display = "block";
+      card.getElementsByTagName('p')[0].style.display = "block"
+      container.classList.remove('container-arrow-after')
+      showEffects = true
+    }
+}
