0625e4ceeb7b3033bb950ca56c48d0c3f3c8bb74
Matheus Juliao <matheusjosejuliao@gmail.com>
2023-04-03 19:05:20 -0300

    Adjust video who we are

---
+++ js/who-we-are.js
@@ -0,0 +1,7 @@
+if(window.innerWidth > 950) {
+    let video = document.getElementById("video");
+    video.style.display = "block"
+} else {
+    let videoMobile = document.getElementById("video-mobile");
+    videoMobile.style.display = "block"
+}
