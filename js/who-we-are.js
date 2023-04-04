3b69aaf38a192666393339504185ae104fb693a8
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-04-03 21:48:21 -0300

    Adjust video mobile

--- js/who-we-are.js
+++ js/who-we-are.js
@@ -1,12 +1,21 @@
-let video = document.createElement("video")
-video.controls = true;
-video.autoplay = true;
+const delay = setInterval(() =>  { 
 
-let classVideo = document.getElementsByClassName("video-biofitness")[0];
-classVideo.appendChild(video)
+    console.log(window.innerWidth)
 
-if(window.innerWidth > 950) { 
-    video.src = "video/venha-treinar-na-biofitness.mp4";
-} else {
-    video.src = "/video/venha-treinar-na-biofitness-mobile.mp4";
-}
+    let video = document.createElement("video")
+    video.controls = true;
+    video.autoplay = true;
+
+    let classVideo = document.getElementsByClassName("video-biofitness")[0];
+    classVideo.appendChild(video)
+
+    if(window.innerWidth > 950) { 
+        video.src = "video/venha-treinar-na-biofitness.mp4";
+    } else {
+        video.src = "/video/venha-treinar-na-biofitness-mobile.mp4";
+    }
+
+    clearInterval(delay);
+
+}, 100);
+
